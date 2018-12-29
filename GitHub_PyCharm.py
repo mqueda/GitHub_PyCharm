153f5a86cfbfec809f9d84e003c56f6255dd133a
mqueda <mqueda@gmail.com>
2018-12-29 10:45:54 +0100

    First File Commit!!! Second File Commit!!! First Directory Commit!!! Third File Commit!!!

--- GitHub_PyCharm.py
+++ GitHub_PyCharm.py
@@ -1,7 +1,8 @@
 def New_Function():
     print("First File Commit!!!")
     print("Second File Commit!!!")
-    print("Directory Commit!!!")
+    print("First Directory Commit!!!")
+    print("Third File Commit!!!")
 
 def main ():
     New_Function()
